e54e6f86a7679f3e5e3a1a61fa61ac24344b9466
Ansh Raj <146376691+AnshRaj112@users.noreply.github.com>
2025-03-21 13:00:55 +0530

    Adding basic search page

---
+++ src/app/search/page.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import Search from "./seach"; // Adjust the path if necessary
+
+export default function Team() {
+  return <Search />;
+}
---
+++ src/app/search/seach.tsx
@@ -0,0 +1,9 @@
+export default function Search() {
+    return (
+      <div>
+        <h1>Search</h1>
+        <p>This is the search page.</p>
+      </div>
+    );
+  }
+  
